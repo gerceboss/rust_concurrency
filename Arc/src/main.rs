1cb348fbffb5f9fdea3d9cfac779fb7574e00f74
Nilav Prajapati <nilavprajapati2004@gmail.com>
2024-06-06 15:04:49 +0530

    added drop trai

--- Arc/src/main.rs
+++ Arc/src/main.rs
@@ -1,9 +1,12 @@
 use std::{ptr::NonNull, sync::atomic::AtomicUsize};
-use std::sync::atomic::Ordering::Relaxed;
+use std::sync::atomic::Ordering::{Release,Acquire,Relaxed};
+use std::ops::Deref;
+use std::sync::atomic::fence;
 struct ArcData<T>{
     ref_count:AtomicUsize,
     data:T,
 }
+// we did not use dorectly Box allocation because we need shared ownershp and not exclusive one
 struct Arc<T>{
     ptr:NonNull<ArcData<T>>,
 }
@@ -29,9 +32,34 @@ impl <T> Arc<T>{
     }
 }
 
+
+impl<T> Deref for Arc<T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        &self.data().data
+    }
+}
+
+
+impl<T> Drop for Arc<T> {
+    fn drop(&mut self) {
+        // we need to make sure that before dropping no one is accessing that data so we reduce the ref_count
+        if self.data().ref_count.fetch_sub(1,Release) == 1 {
+            fence(Acquire);
+            unsafe {
+                drop(Box::from_raw(self.ptr.as_ptr()));
+            }
+        }
+    }
+}
 impl<T> Clone for Arc<T> {
     fn clone(&self) -> Self {
-        // TODO: Handle overflows.
+        
+        // handles overflows
+        if self.data().ref_count.fetch_add(1, Relaxed) > usize::MAX / 2 {
+            std::process::abort();
+        }
         self.data().ref_count.fetch_add(1, Relaxed);
         Arc {
             ptr: self.ptr,
